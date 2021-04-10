e4138283f7a606e13394dbd2fd5d346412814f03
whyroland <42717361+whyroland@users.noreply.github.com>
2021-04-10 13:46:54 -0400

    progress
    break

--- src/app.js
+++ src/app.js
@@ -1,1 +1,42 @@
-// express server
+/**
+ * SETUP
+ */
+// Import libraries
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// Setup server
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+const port = process.env.port || 80;
+app.set('view engine', 'jade');
+app.use(express.static('public'));
+
+/**
+ * WEBPAGES
+ */
+// Default
+app.get("/", function (req, res) {
+    console.log(req.url + "@" + Date() + " User connected to the homepage");
+    res.render('index');
+});
+
+// Appplication
+app.get("/application", function (req, res) {
+    console.log(req.url + "@" + Date() + " User requested to use application");
+    res.render('application');
+});
+
+// Meet the Team
+app.get("/about", function (req, res) {
+    console.log(req.url + "@" + Date() + " User requested to use about");
+    res.render('about');
+});
+
+// Turns on Server
+app.listen(port, function () {
+    console.log("Server has started running on port: " + port);
+});
